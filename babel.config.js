b9eb3e7d5056cf1a3b7393d8a9db2d48b364aa72
Aman Raj <42941876+amanrajtoppoart007@users.noreply.github.com>
2023-11-10 13:56:58 +0530

    initial setup complete

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,38 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets',
+          '@libs': './src/libs',
+          '@state': './src/state',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@database': './src/database',
+          '@config': './src/config',
+          '@navigation': './src/navigation',
+          '@storage': './src/storage',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin', //must be added in the last
+  ],
 };
